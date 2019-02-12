48c5f91a0d0727a4fe4996c20cd348bcc99774f9
Paolo Rondot <paolo.rondot@gmail.com>
2019-02-12 11:42:35 +0100

    version master

--- CodetestGit/main.cpp
+++ CodetestGit/main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Hello!" << endl;
+    cout << "je suis le maitre" << endl;
     return 0;
 }
---
+++ TestCodeBlock/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Arthur" << endl;
+    cout << "C'est Tim" << endl ;
+    cout << "Paolo t'es ou ??" << endl ;
+    cout << "Arthur en galere mdr"<< endl ;
+    cout << "Arthur elle est ou la jalousie maintenant" << endl;
+    return 0;
+}
