2140eae80b39119565c95f152c4a8eba1ae1c70d
ArthurMetzger <arthurmetzger@live.fr>
2019-02-12 11:40:24 +0100

    J'attend

--- TestCodeBlock/main.cpp
+++ TestCodeBlock/main.cpp
@@ -4,26 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout << "Arthur" << endl;
-<<<<<<< HEAD
-    cout << "C'est Tim, un petit con" << endl ;
-=======
-    cout << "C'est Tim" << endl ;
-    cout << "Paolo t'es ou ??" << endl ;
-<<<<<<< HEAD
-<<<<<<< HEAD
-    cout << "PAS LA"
 
-=======
-    cout << "Carine je la pine"<< endl ;
-    cout << "ON LA PINE"<<endl;
-=======
-    cout << "Arthur en galere mdr"<< endl ;
-<<<<<<< HEAD
->>>>>>> 93f5b7e33d19da7632f880f5d5be6f7b6d673591
-=======
-    cout << "Arthur elle est ou la jalousie maintenant ejfizoiejfoiezjf" << endl;
 >>>>>>> c989a6029a5feba261f3621fedfabd5046e326a8
     return 0;
 }
